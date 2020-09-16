2be804ce6b0d333e68260c50505076b1799626bb
= <federicohipper@gmail.com>
2020-09-15 22:44:53 -0300

    agrego bootstrap

--- app-financiapp/src/Hola.js
+++ app-financiapp/src/Hola.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-// import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 class FilaPersona extends Component {
     render() {
