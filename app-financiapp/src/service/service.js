4f540297dfef63c3bfbddfc34d79a4b095fa1dbe
= <federicohipper@gmail.com>
2020-09-25 14:51:31 -0300

    --

--- app-financiapp/src/service/service.js
+++ app-financiapp/src/service/service.js
@@ -9,3 +9,13 @@ export default {
         return fetch(url, requestOptions);
     }
 };
+
+
+// var feriadoService = {
+//     obtenerPorAnio: async function (anio) {
+//         const res = await fetch('http://localhost:3000/api/feriados/' + anio);
+//         const feriados = await res.json();
+//         return feriados;
+//     }
+// }
+// export { feriadoService };
