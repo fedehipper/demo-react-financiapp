46b7a81cf27171ad8fffd07e8e9a4832b3626ec8
= <federicohipper@gmail.com>
2020-09-26 22:40:49 -0300

    creo modal bindeando inputs

--- app-financiapp/src/gastos/ModalNuevoGasto.js
+++ app-financiapp/src/gastos/ModalNuevoGasto.js
@@ -1,12 +1,37 @@
+import moment from 'moment';
 import React, { useEffect, useState } from 'react';
-import { Button, Modal } from 'react-bootstrap';
+import { Button, Form, Modal } from 'react-bootstrap';
 
 function ModalNuevoGasto(props) {
+    const [cantidadPagosDisponibles] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20])
     const [show, setShow] = useState(false);
+    const [valor, setValor] = useState('');
+    const [cantidadPagosSeleccionado, setCantidadPagosSeleccionado] = useState(1);
+    const [fecha, setFecha] = useState(moment(new Date()).format('YYYY-MM-DD'));
+    const [concepto, setConcepto] = useState('');
+
+    const nuevoGasto = () => {
+        return {
+            valor: valor,
+            cantidadPagosSeleccionado: cantidadPagosSeleccionado,
+            fecha: fecha,
+            concepto: concepto,
+            necesario: true
+        }
+    }
+
+    const resetearModal = () => {
+        setValor('');
+        setCantidadPagosSeleccionado(1);
+        setFecha(moment(new Date()).format('YYYY-MM-DD'));
+        setConcepto('');
+    }
 
     const cerrarModal = () => {
         setShow(false);
         props.cerrarModal();
+        // props.crearNuevoGasto(nuevoGasto());
+        resetearModal();
     }
 
     const abrirModal = () => setShow(true);
@@ -23,7 +48,37 @@ function ModalNuevoGasto(props) {
                 <Modal.Header closeButton>
                     <Modal.Title><h5>Nuevo gasto</h5></Modal.Title>
                 </Modal.Header>
-                <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                <Modal.Body>
+                    <Form>
+                        <div className='col pl-0 pr-0'>
+                            <label>Concepto</label>
+                            <input
+                                onChange={e => setConcepto(e.target.value)}
+                                value={concepto}
+                                className='form-control mb-3'>
+                            </input>
+                            <label>Cantidad de pagos</label>
+                            <select
+                                onChange={e => setCantidadPagosSeleccionado(parseInt(e.target.value))}
+                                value={cantidadPagosSeleccionado}
+                                className='form-control mb-3'>
+                                {cantidadPagosDisponibles.map(unaOpcionPago => <option value={unaOpcionPago} key={unaOpcionPago}>{unaOpcionPago}</option>)}
+                            </select>
+                            <label>Monto</label>
+                            <input
+                                onChange={e => setValor(e.target.value)}
+                                value={valor}
+                                className='form-control mb-3'>
+                            </input>
+                            <label>Fecha a realizar el pago</label>
+                            <input
+                                onChange={e => setFecha(e.target.value)}
+                                value={fecha}
+                                className='form-control mb-3'>
+                            </input>
+                        </div>
+                    </Form>
+                </Modal.Body>
                 <Modal.Footer>
                     <Button variant="secondary" onClick={cerrarModal}>Cancelar</Button>
                     <Button variant="primary" onClick={cerrarModal}>Aceptar</Button>
