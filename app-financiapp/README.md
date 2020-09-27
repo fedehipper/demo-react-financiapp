3de223346c2f095dd579c99d45bda76b24e735e9
= <federicohipper@gmail.com>
2020-09-27 01:40:25 -0300

    agrego required

--- app-financiapp/README.md
+++ app-financiapp/README.md
@@ -2,3 +2,6 @@
 
 # Iconos:
 # https://fontawesome.com/how-to-use/on-the-web/using-with/react
+
+# React bootstrap:
+# https://react-bootstrap.github.io/getting-started/introduction/
--- app-financiapp/src/gastos/ModalNuevoGasto.js
+++ app-financiapp/src/gastos/ModalNuevoGasto.js
@@ -1,6 +1,6 @@
 import moment from 'moment';
 import React, { useEffect, useState } from 'react';
-import { Button, Form, Modal } from 'react-bootstrap';
+import { Button, Col, Form, Modal } from 'react-bootstrap';
 
 function ModalNuevoGasto(props) {
     const [cantidadPagosDisponibles] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20])
@@ -27,10 +27,16 @@ function ModalNuevoGasto(props) {
         setConcepto('');
     }
 
-    const cerrarModal = () => {
+    const aceptar = () => {
         setShow(false);
         props.cerrarModal();
         props.crearNuevoGasto(nuevoGasto());
+        resetearModal();
+    }
+
+    const cancelar = () => {
+        setShow(false);
+        props.cerrarModal();
         resetearModal();
     }
 
@@ -48,41 +54,44 @@ function ModalNuevoGasto(props) {
                 <Modal.Header closeButton>
                     <Modal.Title><h5>Nuevo gasto</h5></Modal.Title>
                 </Modal.Header>
-                <Modal.Body>
-                    <Form>
-                        <div className='col pl-0 pr-0'>
-                            <label>Concepto</label>
-                            <input
+                <Form>
+                    <Modal.Body>
+                        <Form.Group as={Col} className='pl-0 pr-0'>
+                            <Form.Label>Concepto</Form.Label>
+                            <Form.Control
+                                required
                                 onChange={e => setConcepto(e.target.value)}
                                 value={concepto}
-                                className='form-control mb-3'>
-                            </input>
-                            <label>Cantidad de pagos</label>
+                                className='mb-3'>
+                            </Form.Control>
+                            <Form.Label>Cantidad de pagos</Form.Label>
                             <select
                                 onChange={e => setCantidadPagos(parseInt(e.target.value))}
                                 value={cantidadPagos}
                                 className='form-control mb-3'>
-                                {cantidadPagosDisponibles.map(unaOpcionPago => <option value={unaOpcionPago} key={unaOpcionPago}>{unaOpcionPago}</option>)}
+                                {cantidadPagosDisponibles
+                                    .map(unaOpcionPago => <option value={unaOpcionPago} key={unaOpcionPago}>{unaOpcionPago}</option>)}
                             </select>
-                            <label>Monto</label>
-                            <input
+                            <Form.Label>Monto</Form.Label>
+                            <Form.Control
+                                required
                                 onChange={e => setValor(e.target.value)}
                                 value={valor}
                                 className='form-control mb-3'>
-                            </input>
-                            <label>Fecha a realizar el pago</label>
-                            <input
+                            </Form.Control>
+                            <Form.Label>Fecha a realizar el pago</Form.Label>
+                            <Form.Control
                                 onChange={e => setFecha(e.target.value)}
                                 value={fecha}
                                 className='form-control mb-3'>
-                            </input>
-                        </div>
-                    </Form>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={cerrarModal}>Cancelar</Button>
-                    <Button variant="primary" onClick={cerrarModal}>Aceptar</Button>
-                </Modal.Footer>
+                            </Form.Control>
+                        </Form.Group>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant='secondary' onClick={cancelar}>Cancelar</Button>
+                        <Button variant='primary' type='submit' onClick={aceptar}>Aceptar</Button>
+                    </Modal.Footer>
+                </Form>
             </Modal>
         </>
     );
