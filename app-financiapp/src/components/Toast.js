ae1ebd735f6d4549667a23d2a996d8d0c27c85c8
= <federicohipper@gmail.com>
2020-10-10 00:19:04 -0300

    refactor

--- app-financiapp/src/components/Toast.js
+++ app-financiapp/src/components/Toast.js
@@ -4,9 +4,7 @@ import { Toast } from 'react-bootstrap';
 function DetalleAccionToast(props) {
     return <Toast onClose={() => props.cerrarToast()} show={props.descripcionToast.esVisible} delay={3000} autohide>
         <Toast.Header>
-            <img src="holder.js/20x20?text=%20" className="rounded mr-2" alt="" />
-            <strong className="mr-auto">Bootstrap</strong>
-            <small>11 mins ago</small>
+            <strong className="mr-auto">Acción realizada</strong>
         </Toast.Header>
         <Toast.Body>{props.descripcionToast.accionRealizada}</Toast.Body>
     </Toast>
