2360a49891d8f6d0e05e74b888ad40447fb52960
= <federicohipper@gmail.com>
2020-10-11 01:19:04 -0300

    reafactor

--- app-financiapp/src/components/gastos/DetalleMensual.js
+++ app-financiapp/src/components/gastos/DetalleMensual.js
@@ -15,40 +15,38 @@ function DetalleMensual(props) {
         />
     }
 
-    return (
-        <Table size="sm" striped hover bordered>
-            <thead className="thead-light text-center">
-                <tr>
-                    <th>Concepto</th>
-                    <th>Fecha</th>
-                    <th>Valor($)</th>
-                    <th>Necesario</th>
-                    <th colSpan='2'>Acciones</th>
+    return <Table size="sm" striped hover bordered>
+        <thead className="thead-light text-center">
+            <tr>
+                <th>Concepto</th>
+                <th>Fecha</th>
+                <th>Valor($)</th>
+                <th>Necesario</th>
+                <th colSpan='2'>Acciones</th>
+            </tr>
+        </thead>
+        <tbody>{
+            props.gastos.map(unGasto => {
+                return <tr key={unGasto.id}>
+                    <td>{unGasto.concepto}</td>
+                    <td className='text-center'>{unGasto.fecha}</td>
+                    <td className='text-right'>{unGasto.valor}</td>
+                    <td className='text-center'>
+                        <div className='form-check'>
+                            <input
+                                type='checkbox'
+                                className='form-check-input'
+                                checked={unGasto.necesario}
+                                onChange={() => props.cambiarNecesidad(unGasto.id)}
+                            />
+                        </div>
+                    </td>
+                    <td>{iconoAccion(faEdit, 'text-primary',() => props.abrirModalEdicionGasto(unGasto),  'Editar gasto')}</td>
+                    <td>{iconoAccion(faTrash, 'text-danger',() => props.abrirModalEliminacionGasto(unGasto.id,unGasto.concepto),  'Eliminar gasto')}</td>
                 </tr>
-            </thead>
-            <tbody>{
-                props.gastos.map(unGasto => {
-                    return <tr key={unGasto.id}>
-                        <td>{unGasto.concepto}</td>
-                        <td className='text-center'>{unGasto.fecha}</td>
-                        <td className='text-right'>{unGasto.valor}</td>
-                        <td className='text-center'>
-                            <div className='form-check'>
-                                <input
-                                    type='checkbox'
-                                    className='form-check-input'
-                                    checked={unGasto.necesario}
-                                    onChange={() => props.cambiarNecesidad(unGasto.id)}
-                                />
-                            </div>
-                        </td>
-                        <td>{iconoAccion(faEdit, 'text-primary',() => props.abrirModalEdicionGasto(unGasto),  'Editar gasto')}</td>
-                        <td>{iconoAccion(faTrash, 'text-danger',() => props.abrirModalEliminacionGasto(unGasto.id,unGasto.concepto),  'Eliminar gasto')}</td>
-                    </tr>
-                })}
-            </tbody>
-        </Table >
-    );
+            })}
+        </tbody>
+    </Table >
 }
 
 export default DetalleMensual;
